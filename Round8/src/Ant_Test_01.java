e3bb08c266c3073459d48a5d58c67a3da75a0675
최민준 <@LAPTOP-KHS2ML49>
2021-03-17 15:17:14 +0900

    ant

---
+++ Round8/src/Ant_Test_01.java
@@ -0,0 +1,6 @@
+
+public class Ant_Test_01 {
+	public static void main(String[] ar) {
+		System.out.println("Ant_Test_01!!!");
+	}
+}
---
+++ Round8/src/Ant_Test_02.java
@@ -0,0 +1,6 @@
+
+public class Ant_Test_02 {
+	public static void main(String[] ar) {
+		System.out.println("Ant_Test_02!!!");
+	}
+}
